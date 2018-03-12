3bea3ff914e4d4e052ce051bb41eb995ec46fb21
Dmitry Tarantin <dimichspb@gmail.com>
2018-03-12 13:40:35 +0300

    Namespaces fix

--- src/repositories/ActiveRecordNotificationTypeRepository.php
+++ src/repositories/ActiveRecordNotificationTypeRepository.php
@@ -2,7 +2,7 @@
 namespace dimichspb\yii\notificator\repositories;
 
 use dimichspb\yii\notificator\interfaces\NotificationTypeInterface;
-use dimichspb\yii\notificator\models\Notification\search\NotificationTypeSearch;
+use dimichspb\yii\notificator\models\NotificationType\search\NotificationTypeSearch;
 use dimichspb\yii\notificator\models\NotificationType\Id;
 use dimichspb\yii\notificator\models\NotificationType\NotificationType;
 use yii\data\DataProviderInterface;
